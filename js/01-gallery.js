1faaa560bfffe242f88e91e5c8ccc6299ce679f5
owin66 <78900270+owin66@users.noreply.github.com>
2022-12-24 21:19:28 +0200

    work

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,16 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+function renderImg(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `<div class='gallery__item'>
+    <img class='gallery__image' src='${preview}' alt='${description}'>
+    </div>`;
+    })
+    .join('');
+}
+console.log(galleryEl);
+const cards = renderImg(galleryItems);
+galleryEl.insertAdjacentHTML('afterBegin', cards);

28a0850cce3b018f769f804945e18f08494c7d37
owin66 <78900270+owin66@users.noreply.github.com>
2022-12-24 22:18:10 +0200

    ref

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,16 +1,36 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 const galleryEl = document.querySelector('.gallery');
+
+const cards = renderImg(galleryItems);
+
+galleryEl.insertAdjacentHTML('afterBegin', cards);
+
 function renderImg(gallery) {
   return gallery
     .map(({ preview, original, description }) => {
       return `<div class='gallery__item'>
-    <img class='gallery__image' src='${preview}' alt='${description}'>
+      <a class = 'gallery__link' href = '${original}'>
+    <img class='gallery__image' src='${preview}' alt='${description}' data-source='${original}'>
+    </a>
     </div>`;
     })
     .join('');
 }
 console.log(galleryEl);
-const cards = renderImg(galleryItems);
-galleryEl.insertAdjacentHTML('afterBegin', cards);
+
+const instance = basicLightbox.create(`<img class="modal-img src="">`, {
+  onShow: instance => {
+    window.addEventListener('keydown', onEscCloseModal);
+  },
+
+  onClose: instance => {
+    window.removeEventListener('keydown', onEscCloseModal);
+  },
+});
+
+function openImageClick(e) {
+  console.log(e);
+}
+
+galleryEl.addEventListener('click', openImageClick);

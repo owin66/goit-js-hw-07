b86e936f298bbb58c348243b6e835c5a1c24b190
owin66 <78900270+owin66@users.noreply.github.com>
2022-12-25 10:51:39 +0200

    finish02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const cards = renderImg(galleryItems);
+
+galleryEl.insertAdjacentHTML('afterBegin', cards);
+
+function renderImg(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `<a class = 'gallery__item' href = '${original}'>
+    <img class='gallery__image' src='${preview}' alt='${description}'>
+    </a>`;
+    })
+    .join('');
+}
+
+console.log(galleryEl);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionSelector: 'img',
+  captionsData: 'alt',
+  captionType: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
